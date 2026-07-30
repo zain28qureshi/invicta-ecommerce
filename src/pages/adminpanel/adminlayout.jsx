c5095b1ac739e16eb2e865420ed2df64784e1f7e
zain28qureshi <zain28qureshi@gmail.com>
2026-07-30 10:05:50 +0500

    correcting the mistake

--- src/pages/adminpanel/adminlayout.jsx
+++ src/pages/adminpanel/adminlayout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Sidebar from "../../components/admin/sidebar.jsx";
-import SidebarItem from "../../components/admin/sidebaritem.jsx";
+import SidebarItem from "../../components/admin/SidebarItem.jsx";
 
 import {
   BaggageClaim,
